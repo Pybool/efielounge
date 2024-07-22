76a57477bab802827ce2618db27c93b8bb25f325
Pybool <ekoemmanueljavl@gmail.com>
2024-07-22 12:42:20 +0100

    Feat: Minor bug fixes

--- efielounge-backend/ecosystem.config.js
+++ efielounge-backend/ecosystem.config.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.prod' });
 
 module.exports = {
   apps: [
     {
-      name: "crygoca",
-      script: "./dist/index.js",
+      name: "efielounge",
+      script: "./dist/server.js",
       env_production: {
         NODE_ENV: "prod",
         ...process.env
