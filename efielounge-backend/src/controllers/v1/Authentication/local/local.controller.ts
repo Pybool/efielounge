aace0f0a0bddf7f0e4f47c74561a4ebf0b94ce31
Pybool <ekoemmanueljavl@gmail.com>
2024-08-15 09:01:26 +0100

    Merged registration and login

--- efielounge-backend/src/controllers/v1/Authentication/local/local.controller.ts
+++ efielounge-backend/src/controllers/v1/Authentication/local/local.controller.ts
@@ -5,26 +5,26 @@ import Xrequest from "../../../../interfaces/extensions.interface";
 import message from "../../../../helpers/messages";
 
 const authController: any = {
-    // createAccount: async (req: Xrequest, res: Response, next: NextFunction) => {
-    //   let status = 200;
-    //   try {
-    //     const authentication = new Authentication(req);
-    //     const result = await authentication.createAccount();
-    //     if (result.status) {
-    //       status = 201;
-    //       res.status(status).json(result);
-    //     } else {
-    //       console.log("result ", result);
-    //       return res.status(200).json(result);
-    //     }
-    //   } catch (error: any) {
-    //     console.log("Auth error ", error.message);
-    //     if (error.isJoi === true) {
-    //       error.status = 422;
-    //     }
-    //     res.status(status).json({ status: false, message: error?.message });
-    //   }
-    // },
+    createAccount: async (req: Xrequest, res: Response, next: NextFunction) => {
+      let status = 200;
+      try {
+        const authentication = new Authentication(req);
+        const result = await authentication.createAccount();
+        if (result.status) {
+          status = 201;
+          res.status(status).json(result);
+        } else {
+          console.log("result ", result);
+          return res.status(200).json(result);
+        }
+      } catch (error: any) {
+        console.log("Auth error ", error.message);
+        if (error.isJoi === true) {
+          error.status = 422;
+        }
+        res.status(status).json({ status: false, message: error?.message });
+      }
+    },
 
     // phoneRegister: async (req: Xrequest, res: Response, next: NextFunction) => {
     //   let status = 200;
@@ -154,100 +154,100 @@ const authController: any = {
     },
 
   
-    // sendPasswordResetLink: async (
-    //   req: Xrequest,
-    //   res: Response,
-    //   next: NextFunction
-    // ) => {
-    //   try {
-    //     let status = 200;
-    //     const authentication = new Authentication(req);
-    //     const result:any = await authentication.sendPasswordResetLink();
-    //     res.status(status).json(result);
-    //   } catch (error: any) {
-    //     if (error.isJoi === true) error.status = 422;
-    //     next(error);
-    //   }
-    // },
+    sendPasswordResetLink: async (
+      req: Xrequest,
+      res: Response,
+      next: NextFunction
+    ) => {
+      try {
+        let status = 200;
+        const authentication = new Authentication(req);
+        const result:any = await authentication.sendPasswordResetLink();
+        res.status(status).json(result);
+      } catch (error: any) {
+        if (error.isJoi === true) error.status = 422;
+        next(error);
+      }
+    },
   
-    // sendEmailConfirmationOtp: async (
-    //   req: Xrequest,
-    //   res: Response,
-    //   next: NextFunction
-    // ) => {
-    //   try {
-    //     let status = 200;
-    //     const authentication = new Authentication(req);
-    //     const result: any = await authentication.sendEmailConfirmationOtp();
-    //     if (!result.status) status = 422;
-    //     res.status(status).json(result);
-    //   } catch (error: any) {
-    //     if (error.isJoi === true) error.status = 422;
-    //     next(error);
-    //   }
-    // },
+    sendEmailConfirmationOtp: async (
+      req: Xrequest,
+      res: Response,
+      next: NextFunction
+    ) => {
+      try {
+        let status = 200;
+        const authentication = new Authentication(req);
+        const result: any = await authentication.sendEmailConfirmationOtp();
+        if (!result.status) status = 422;
+        res.status(status).json(result);
+      } catch (error: any) {
+        if (error.isJoi === true) error.status = 422;
+        next(error);
+      }
+    },
   
-    // resetPassword: async (req: Xrequest, res: Response, next: NextFunction) => {
-    //   console.log("reset password token ", req.query.token);
-    //   try {
-    //     let status = 200;
-    //     const authentication = new Authentication(req);
-    //     const result = await authentication.resetPassword();
-    //     res.status(status).json(result);
-    //   } catch (error: any) {
-    //     if (error.isJoi === true) error.status = 422;
-    //     next(error);
-    //   }
-    // },
+    resetPassword: async (req: Xrequest, res: Response, next: NextFunction) => {
+      console.log("reset password token ", req.query.token);
+      try {
+        let status = 200;
+        const authentication = new Authentication(req);
+        const result = await authentication.resetPassword();
+        res.status(status).json(result);
+      } catch (error: any) {
+        if (error.isJoi === true) error.status = 422;
+        next(error);
+      }
+    },
   
-    // verifyAccountEmail: async (req: Xrequest, res: Response, next: NextFunction) => {
-    //   try {
-    //     let status = 400;
-    //     const authentication = new Authentication(req);
-    //     const result = await authentication.verifyAccountEmail();
-    //     if (result.status) status = 200;
-    //     res.status(status).json(result);
-    //   } catch (error: any) {
-    //     error.status = 422;
-    //     next(error);
-    //   }
-    // },
+    verifyAccountEmail: async (req: Xrequest, res: Response, next: NextFunction) => {
+      try {
+        let status = 400;
+        const authentication = new Authentication(req);
+        const result = await authentication.verifyAccountEmail();
+        if (result.status) status = 200;
+        res.status(status).json(result);
+      } catch (error: any) {
+        error.status = 422;
+        next(error);
+      }
+    },
   
-    // loginAccount: async (req: Xrequest, res: Response, next: NextFunction) => {
-    //   try {
-    //     let status = 400;
-    //     const authentication = new Authentication(req);
-    //     const result = await authentication.loginAccount();
-    //     status = 200;
-    //     return res.status(status).json(result);
-    //   } catch (error: any) {
-    //     if (error.isJoi === true) {
-    //       res
-    //         .status(400)
-    //         .json({ status: false, message: message.auth.invalidCredentials });
-    //     } else {
-    //       res
-    //         .status(400)
-    //         .json({ status: false, message: "Could not process login request!" });
-    //     }
-    //   }
-    // },
+    loginAccount: async (req: Xrequest, res: Response, next: NextFunction) => {
+      try {
+        let status = 400;
+        const authentication = new Authentication(req);
+        const result = await authentication.loginAccount();
+        status = 200;
+        return res.status(status).json(result);
+      } catch (error: any) {
+        if (error.isJoi === true) {
+          res
+            .status(400)
+            .json({ status: false, message: message.auth.invalidCredentials });
+        } else {
+          res
+            .status(400)
+            .json({ status: false, message: "Could not process login request!" });
+        }
+      }
+    },
   
-    // getRefreshToken: async (req: Xrequest, res: Response, next: NextFunction) => {
-    //   try {
-    //     let status = 400;
-    //     const authentication = new Authentication(req);
-    //     if (req.body.refreshToken == "") {
-    //       res.status(200).json({ status: false });
-    //     }
-    //     const result = await authentication.getRefreshToken(next);
-    //     if (result) status = 200;
-    //     res.status(status).json(result);
-    //   } catch (error: any) {
-    //     error.status = 422;
-    //     next(error);
-    //   }
-    // },
+    getRefreshToken: async (req: Xrequest, res: Response, next: NextFunction) => {
+      try {
+        let status = 400;
+        const authentication = new Authentication(req);
+        if (req.body.refreshToken == "") {
+          res.status(200).json({ status: false });
+        }
+        const result = await authentication.getRefreshToken(next);
+        if (result) status = 200;
+        res.status(status).json(result);
+      } catch (error: any) {
+        error.status = 422;
+        next(error);
+      }
+    },
   };
 
 export default authController;
