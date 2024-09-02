7ea29679b101ec592c010ae0c89d2320e2070803
Pybool <ekoemmanueljavl@gmail.com>
2024-09-02 19:19:10 +0100

    Added google place addresses

--- website-efielounge/src/environments/environment.ts
+++ website-efielounge/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   api: 'https://be.efielounge.com',
   // api: 'http://localhost:8200',
   AES_SECRET_KEY: 'XXXX-XXXX-XXXX-XXXX',
-  GOOGLE_PLACES_API_KEY:"AIzaSyBy4Iz1KlW17yIYn-8wxQR-M8ptroS9Baw"
+  GOOGLE_PLACES_API_KEY:""
 };
 
